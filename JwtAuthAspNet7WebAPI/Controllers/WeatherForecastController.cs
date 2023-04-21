074c6b39ef53b734c3766b1e333898066a2787dd
NDE MAMBOU AMEDE CLINTON <cmambou31@gmail.com>
2023-04-21 21:25:39 +0100

    Refactory WeatherForecastController

--- JwtAuthAspNet7WebAPI/Controllers/WeatherForecastController.cs
+++ JwtAuthAspNet7WebAPI/Controllers/WeatherForecastController.cs
@@ -13,21 +13,12 @@ namespace JwtAuthAspNet7WebAPI.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok(Summaries);
         }
     }
 }
